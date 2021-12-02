697a26758fb6e7390b7ff8aafd2b3d51a59904dc
Howard Kwon <howie.ykwon70@gmail.com>
2021-12-02 00:39:12 -0500

    complete anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    // Check to see if same length
+    if (stringA.length !== stringB.length) {
+        return false;
+    }
+
+    // See if strings are anagrams of eachother
+    // Break strings down to see if they have same chars and same quantity
+        // Create object for breakdown
+        // Create loop to breakdown string and a loop to compare
+    let lookup = {};
+
+    for (let i = 0; i < stringA.length; i++) {
+        let letter = stringA[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    // Then compare the strings
+    for (let i = 0; i < stringB.length; i++) {
+        let letter = stringB[i];
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true;
+}
+
+console.log(anagrams("spell", "pells"));
+console.log(anagrams('Hi there', 'Bye there'));
 
 module.exports = anagrams;
